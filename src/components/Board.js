b52b0a1d5dcdffe927ae5fbed00e9debc064341e
rishi1606 <85154647+rishi1606@users.noreply.github.com>
2022-01-03 13:29:44 +0530

    Update Board.js

--- src/components/Board.js
+++ src/components/Board.js
@@ -11,9 +11,7 @@ const style = {
 	gridTemplate: "repeat(3, 1fr) / repeat(3, 1fr)",
 };
 
-// props has 'value' which is a array of 9 charectors that needs to de displayed in box.js
-// and a 'onClick' function that needs to be called when button is clicked
-// restoring array of box.jsx
+
 const Board = (props) => (
     <div style={style}>
     {[ ...Array(9)].map((_, pos) => <Box key={pos} name={pos} onClick={()=>props.onClick(pos)} value={props.value[pos]}/>)}
